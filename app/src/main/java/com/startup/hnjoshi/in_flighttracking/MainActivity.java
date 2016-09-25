f11cba4afda165fc2841e381583a7d91045e4fda
Hardik Joshi <hnjoshi@live.com>
2016-09-25 00:55:18 -0700

    retrieve stored settings function added

--- app/src/main/java/com/startup/hnjoshi/in_flighttracking/MainActivity.java
+++ app/src/main/java/com/startup/hnjoshi/in_flighttracking/MainActivity.java
@@ -1,9 +1,12 @@
 package com.startup.hnjoshi.in_flighttracking;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -13,11 +16,24 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 import static android.R.attr.value;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    public static final String DEFAULT="N/A";
+
+    // texts that changes dynamically
+    private TextView accuracy;
+    private TextView speed;
+    private TextView altitude;
+
+    // user preferences variable, initially it will be all default values
+    private String speed_unit = "imperial";
+    private String altitude_unit = "imperial";
+    private int update_interval = 3;    // 3 sec
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,6 +59,33 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+
+        accuracy = (TextView)findViewById(R.id.accuracyTextView);
+        speed = (TextView)findViewById(R.id.speedTextView);
+        altitude = (TextView)findViewById(R.id.altitudeTextView);
+    }
+
+    // When user starts to see the UI
+    @Override
+    protected void onStart(){
+        super.onStart();
+
+
+    }
+
+    // When user return to the activity
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // 1. Load previously stored settings or newly modified settings
+        loadSettings();
+        // Tester - working verified
+        Log.d("Speed Unit", this.speed_unit);
+        Log.d("Altitude Unit", this.altitude_unit);
+        Log.d("Update interval", this.update_interval+"");
+
     }
 
     @Override
@@ -110,5 +153,33 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    // Loading user Settings if available
+    private void loadSettings(){
+        //SharedPreference Storage for user preferred settings
+        SharedPreferences sharedPreferences = getSharedPreferences("In-flightTrackingData", Context.MODE_PRIVATE);
+        // if the key does not exist or unavailable then DEFAULT value is assigned to variable
+        // if the key exist or available then its corresponding value is assigned to variable
+        String speed_unit = sharedPreferences.getString("speed_unit", DEFAULT);
+        String altitude_unit = sharedPreferences.getString("altitude_unit", DEFAULT);
+        String update_interval = sharedPreferences.getString("update_interval", DEFAULT);
+
+        // 1. DEFAULT value check - User may not have accessed settings yet
+        // 2. Stored value check - User may have modified speed unit settings
+        if(!speed_unit.equals(DEFAULT) && !speed_unit.equals(this.speed_unit)){
+            this.speed_unit = speed_unit;
+        }
+
+        // 1. DEFAULT value check - User may not have accessed settings yet
+        // 2. Stored value check - User may have modified altitude unit settings
+        if(!altitude_unit.equals(DEFAULT) && !altitude_unit.equals(this.altitude_unit)){
+            this.altitude_unit = altitude_unit;
+        }
+        // 1. DEFAULT value check - User may not have accessed settings yet
+        // 2. Stored value check - User may have modified update interval settings
+        if(!update_interval.equals(DEFAULT) && !update_interval.equals(this.update_interval)){
+            this.update_interval = Integer.parseInt(update_interval);
+        }
     }
 }
